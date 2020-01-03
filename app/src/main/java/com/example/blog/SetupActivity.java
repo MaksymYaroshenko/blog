6c7206d4d03d952ad175d93049204c28eca54ff3
MaksymYaroshenko <53305575+MaksymYaroshenko@users.noreply.github.com>
2020-01-03 18:24:22 +0200

    Add account settings permission

--- app/src/main/java/com/example/blog/SetupActivity.java
+++ app/src/main/java/com/example/blog/SetupActivity.java
@@ -1,10 +1,22 @@
 package com.example.blog;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private CircleImageView setupImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +26,23 @@ public class SetupActivity extends AppCompatActivity {
         Toolbar setupToolbar = findViewById(R.id.setupToolbar);
         setSupportActionBar(setupToolbar);
         getSupportActionBar().setTitle("Account Settings");
+
+        setupImage = findViewById(R.id.setupImage);
+        setupImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                    if (ContextCompat.checkSelfPermission(SetupActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                        Toast.makeText(SetupActivity.this, "Permission denied", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(SetupActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    } else {
+                        Toast.makeText(SetupActivity.this, "You have permission", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 }
